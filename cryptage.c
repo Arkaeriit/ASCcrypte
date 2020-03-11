581db97233ec1b09d6cd4a144c69eba3e827cbdf
Arkaeriit <maxbouillot@gmail.com>
2020-03-11 20:52:47 +0100

    Réécriture des algo de cryptage.

--- cryptage.c
+++ cryptage.c
@@ -1,52 +1,59 @@
 #include "cryptage.h"
 
-
 int cryptage(lua_State *L){
     const char* clair = luaL_checkstring(L,1);
     const char* password = luaL_checkstring(L,2);
     long int len = luaL_checkinteger(L,3);
-    unsigned int lenPsw = luaL_checkinteger(L,4);
     char* result = malloc(sizeof(char) * len);
+    XOSHIROetat* state = maxihash(password);
+    XOSHIRO_importState(*state);
+    free(state);
     for(int i = 0;i<len;i++){
-        *(result + i) = *(clair + i) ^ *(password + i%lenPsw);
+        *(result + i) = *(clair + i) ^ (char) XOSHIRO_rand();
     }
     lua_pushlstring(L,result,len);
     free(result);
     return 1;
 }
 
-int passwordGenerator(lua_State *L){
-    const char* psw = luaL_checkstring(L,1);
-    unsigned int len = luaL_checkinteger(L,2);
-    long int pswSum = 0;
-    for(int i=0;i<len;i++){
-        pswSum = pswSum + ((unsigned int) *(psw + i) * i%150) ;//Multiplier le char actuel par i%150 permet de faire en sorte que deux palindromes ont des sommes différentes
-    }
-    XOSHIRO_seed(pswSum + len * *(psw)); //La somme du mot de passe va nous permettre de choisir le début du hasard.
-    unsigned int lenTotal = (4096 + pswSum * (22 + minirand(34))) * (1 + minirand(10)); //La taille du mot de passe allongé est aussi aléatoire
-    if(lenTotal > (500000000 * minirand(5))){
-        lenTotal = 500000000 * minirand(5) + 4096 * minirand(255);
-    }
-    char* ret = malloc(sizeof(char) * lenTotal);
-    unsigned int proportionContenu = minirand(75); //fraction des charactères qui proviènnent dirrectement du mot de passe source
-    for(int i=0;i<lenTotal;i++){
-        if(i%(minirand(proportionContenu) + 1) == 0){
-            unsigned int place = minirand(len);
-            *(ret + i) = *(psw + place); //On prend un élément du mot de passe au pif et on le met à la suite.
-            proportionContenu = minirand(75);
-        }else{
-            *(ret + i) = (char) minirand(256);
-        }
-    }
-    lua_pushlstring(L,ret,lenTotal);
-    free(ret);
-    return 1;
+int crypte_file(const char* in,const char* out, const char* password){
+    //Initialisation des pointeurs de fichier
+    FILE *fin,*fout;
+    if( (fin = fopen(in,"r")) == NULL)
+        return 1;
+    if( (fout = fopen(out,"w")) == NULL)
+        return 2;
+    //Calcul de la taille des fichiers
+    fseek(fin, 0, SEEK_END);
+    size_t size = ftell(fin);
+    fseek(fin, 0, SEEK_SET);
+    //Initialisation du rng
+    XOSHIROetat* state = maxihash(password); 
+    XOSHIRO_importState(*state);
+    free(state);
+    //Cryptage
+    for(size_t i=0; i<size; i++)
+        fputc( fgetc(fin) ^ (char) XOSHIRO_rand(), fout);
+    //Fermeture des fichiers
+    fclose(fout);
+    fclose(fin);
+    return 0;
+}
+
+int crypte_file_lua(lua_State* L){
+    const char* in = luaL_checkstring(L,1);
+    const char* out = luaL_checkstring(L,2);
+    const char* password = luaL_checkstring(L,3);
+    int code = crypte_file(in,out,password);
+    if(code)
+        fprintf(stderr, "Error : while encrypting file.\nError code : %i.\n",code);
+    return 0;
 }
 
 void CR_include(lua_State *L){
     lua_pushcfunction(L,cryptage);
     lua_setglobal(L,"C_cryptage");
-    lua_pushcfunction(L,passwordGenerator);
-    lua_setglobal(L,"passwordGenerator");
+    lua_pushcfunction(L,crypte_file_lua);
+    lua_setglobal(L,"crypte_file");
 }
 
--- cryptage.h
+++ cryptage.h
@@ -4,6 +4,8 @@
 |lua_State.                                                     |
 \--------------------------------------------------------------*/
 
+#ifndef CRYPTAGE
+#define CRYPTAGE
 
 #include <lua.h>
 #include <lualib.h>
@@ -11,9 +13,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include "rng.h"
+#include "hash.h"
 
-int cryptage(lua_State *L); //Reçoit deux chaines et la longueur de la première, resort un xor des deux
-int passwordGenerator(lua_State *L); //Reçoit une chaine et sa longueur, renvoie un mot de passe de 4096 charactères 
+int cryptage(lua_State *L); //Reçoit deux chaines et la longueur de la première, resort la première cryptée
+int crypte_file(const char* in, const char* out, const char* password); //Crypte le contenu du fichier au chemin in vers le ficher ou chemin out. Retourne 1 ou 2 en cas d'erreur.
+int crypte_file_lua(lua_State* L); //Warper en lua de crypte_file
 
 void CR_include(lua_State *L); //Permet de mettre la fonction d'au dessu dans le lua_State
+
+#endif
 
